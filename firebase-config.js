ae5f59df70b0a34acf231165ce0898ee0801e0ff
2524 <yotaoku0307@gmail.com>
2025-07-16 22:40:06 +0900

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAweBQJ17LYvPsfYUCBLygwdehoTEOrsiQ",
+  authDomain: "project-7411251555753678403.firebaseapp.com",
+  projectId: "project-7411251555753678403",
+  storageBucket: "project-7411251555753678403.appspot.com",
+  messagingSenderId: "172462885590",
+  appId: "1:172462885590:web:bf2ca6b6e7e668f1445777"
+};
